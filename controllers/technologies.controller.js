b6d6198d46bf5c290527738f042d15aca9555422
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-09-15 16:09:56 +0300

    Add technologies controllers

--- controllers/technologies.controller.js
+++ controllers/technologies.controller.js
@@ -1,1 +1,107 @@
 const Technology = require('../models/technologies.model');
+const getImageFileType = require('../utils/getImageFileType');
+const sanitize = require('mongo-sanitize');
+const fs = require('fs');
+
+exports.getAllTechnologies = async (req, res) => {
+  try {
+    res.json(await Technology.find());
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getTechnologyById = async (req, res) => {
+  try {
+    const technology = await Technology.findById(req.params.id);
+    if (!technology) {
+      return res.status(404).json({ message: 'Not found...' });
+    }
+    res.json(technology);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getTechnologyBySearch = async (req, res) => {
+  try {
+    const technology = await Technology.find({
+      $text: { $search: req.params.searchPhrase },
+    });
+    if (!technology) {
+      return res.status(404).json({ message: 'Not found...' });
+    }
+    res.json(technology);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.addTechnology = async (req, res) => {
+  const { title, level } = sanitize(req.body);
+
+  const fileType = req.file ? await getImageFileType(req.file) : 'unknown';
+  const isFile =
+    req.file &&
+    ['image/png', 'image/jpg', 'image/jpeg', 'image/git'].includes(fileType);
+
+  const isDataValid = title && level && isFile;
+
+  try {
+    if (isDataValid) {
+      const newTechnology = new Technology({
+        image: req.file.filename,
+        title: title,
+        level: level,
+      });
+      await newTechnology.save();
+      res.json(newTechnology);
+    } else {
+      if (req.file) {
+        fs.unlinkSync(`./public/uploads/${req.file.filename}`);
+      }
+      res.status(400).json({ message: 'Bad request' });
+    }
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.updateTechnology = async (req, res) => {
+  const { title, level } = req.body;
+  try {
+    const technology = await Technology.findById(req.params.id);
+    if (!technology) {
+      if (req.file) {
+        fs.unlinkSync(`./public/uploads/${req.file.filename}`);
+      }
+      res.status(400).json({ message: 'Bad request' });
+    }
+    await Technology.updateOne(
+      { _id: req.params.id },
+      {
+        $set: {
+          image: req.file.filename,
+          title: title,
+          level: level,
+        },
+      }
+    );
+    res.json({ message: 'OK' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.removeTechnologyById = async (req, res) => {
+  try {
+    const technology = await Technology.findById(req.params.id);
+    if (!technology) {
+      return res.status(404).json({ message: 'Not found...' });
+    }
+    await Technology.deleteOne({ _id: req.params.id });
+    res.json({ message });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
--- routes/technologies.routes.js
+++ routes/technologies.routes.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const imageUpload = require('../utils/imageUpload');
+const {
+  getAllTechnologies,
+  getTechnologyById,
+  getTechnologyBySearch,
+  addTechnology,
+  updateTechnology,
+  removeTechnologyById,
+} = require('../controllers/technologies.controller');
 
-router.get('/technologies');
-router.get('/technologies/:id');
-router.get('/technologies/search/:searchPhrase');
-router.post('/technologies', imageUpload.single('image'));
-router.put('/technologies/:id', imageUpload.single('image'));
-router.delete('/technologies/:id');
+router.get('/technologies', getAllTechnologies);
+router.get('/technologies/:id', getTechnologyById);
+router.get('/technologies/search/:searchPhrase', getTechnologyBySearch);
+router.post('/technologies', imageUpload.single('image'), addTechnology);
+router.put('/technologies/:id', imageUpload.single('image'), updateTechnology);
+router.delete('/technologies/:id', removeTechnologyById);
 
 module.exports = router;
