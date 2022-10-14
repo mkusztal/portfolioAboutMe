27565829d744ae1680996277a46094ab68e28db8
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-10-14 18:30:07 +0300

    Change HomePage from boostrap to css

--- client/src/components/pages/HomePage/HomePage.js
+++ client/src/components/pages/HomePage/HomePage.js
@@ -1,24 +1,27 @@
-import { Col, Row, Button, Image } from 'react-bootstrap';
+import { Col, Row, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import styles from './HomePage.module.scss';
 const HomePage = () => {
   return (
     <div className={styles.root}>
-      <Row>
-        <Col>
-          <Image src="pobracZRedux" />
-        </Col>
-
-        <Col>
-          <h3 className={styles.header}>Hi! I'm Maciej Kusztal</h3>
-          <p className={styles.text}>
-            I'm Junior Full Stack Web Developer who is looking for Junior Backend Developer...
+      <div className={styles.leftColumn}>
+        <img src="/images/me.png" alt="me" />
+      </div>
+      <div className={styles.rightColumn}>
+        <div className={styles.text}>
+          <h3>
+            Hi! I'm <span>Maciej Kusztal</span>
+          </h3>
+          <p>
+            I'm Junior Full Stack Web Developer who is looking for Junior
+            Backend Developer...
           </p>
-          <Link to={'/aboutme'}>
-            <Button variant="warning">Read more</Button>
-          </Link>
-        </Col>
-      </Row>
+        </div>
+
+        <Link className={styles.button} to={'/aboutme'}>
+          <Button variant="warning">Read more</Button>
+        </Link>
+      </div>
     </div>
   );
 };
