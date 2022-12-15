9fdf90749f6476e29744794bf76ceb0541287468
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-12-15 17:19:14 +0200

    Add photos to carousel

--- client/src/components/features/SingleProjectCard/SingleProjectCard.js
+++ client/src/components/features/SingleProjectCard/SingleProjectCard.js
@@ -5,17 +5,15 @@ const SingleProjectCard = ({ name, description, technologies, linkGitHub }) => {
   return (
     <section className={styles.root}>
       <div className={styles.card}>
-        <div className={styles.card_project}>
-          <h1 className={styles.title}>{name}</h1>
-          <div className={styles.description}>{description}</div>
-          <div className={styles.technologies}>
-            Used technologies: <span>{technologies}</span>
-          </div>
-          <div className={styles.links}>
-            <a href={linkGitHub} target="_blank" rel="noreferrer">
-              <FaGithub />
-            </a>
-          </div>
+        <h1 className={styles.title}>{name}</h1>
+        <div className={styles.description}>{description}</div>
+        <div className={styles.technologies}>
+          Used technologies: <span>{technologies}</span>
+        </div>
+        <div className={styles.links}>
+          <a href={linkGitHub} target="_blank" rel="noreferrer">
+            <FaGithub />
+          </a>
         </div>
       </div>
     </section>
--- client/src/components/features/TechnologiesCarousel/TechnologyData.js
+++ client/src/components/features/TechnologiesCarousel/TechnologyData.js
@@ -1,38 +1,38 @@
 export const TechnologyData = [
   {
-    image: "https://assets.stickpng.com/thumbs/5847f5c3cef1014c0b5e489d.png",
+    image: "/technologies/html.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/62b2221b038aad4d3ed7ca30.png",
+    image: "/technologies/css.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/62a74dd1223343fbc2207d00.png",
+    image: "/technologies/react.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/5848309bcef1014c0b5e4a9a.png",
+    image: "/technologies/redux.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/62a76492bd73a4af5c5d4fb9.png",
+    image: "/technologies/bootstrap.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/62debc37ff3c6e4b8b5de8d2.png",
+    image: "/technologies/mysql.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/58481021cef1014c0b5e494b.png",
+    image: "/technologies/mongoDB.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/62a754b9223343fbc2207d0a.png",
+    image: "/technologies/npm.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/62a75261223343fbc2207d09.png",
+    image: "/technologies/nodeJS.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/613b64fe30e8530004ba3a03.png",
+    image: "/technologies/javascript.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/5847f981cef1014c0b5e48be.png",
+    image: "/technologies/git.png",
   },
   {
-    image: "https://assets.stickpng.com/thumbs/5847f98fcef1014c0b5e48c0.png",
+    image: "/technologies/gitHub.png",
   },
 ];
