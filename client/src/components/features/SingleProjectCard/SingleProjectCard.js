9fa9f7b888df4972f663115f6c676927f28aa725
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2022-12-15 16:51:40 +0200

    Basic design of single page project

--- client/src/components/features/SingleProjectCard/SingleProjectCard.js
+++ client/src/components/features/SingleProjectCard/SingleProjectCard.js
@@ -1,4 +1,5 @@
 import styles from "./SingleProjectCard.module.scss";
+import { FaGithub } from "react-icons/fa";
 
 const SingleProjectCard = ({ name, description, technologies, linkGitHub }) => {
   return (
@@ -7,8 +8,14 @@ const SingleProjectCard = ({ name, description, technologies, linkGitHub }) => {
         <div className={styles.card_project}>
           <h1 className={styles.title}>{name}</h1>
           <div className={styles.description}>{description}</div>
-          <div className={styles.technologies}>{technologies}</div>
-          <div className={styles.links}>{linkGitHub}</div>
+          <div className={styles.technologies}>
+            Used technologies: <span>{technologies}</span>
+          </div>
+          <div className={styles.links}>
+            <a href={linkGitHub} target="_blank" rel="noreferrer">
+              <FaGithub />
+            </a>
+          </div>
         </div>
       </div>
     </section>
