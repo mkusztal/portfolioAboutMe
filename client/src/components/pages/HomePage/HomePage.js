e9821d743d1558ab43992790fc44e66677eb4618
Maciej Kusztal <maciej.kusztal1995@gmail.com>
2023-01-11 21:27:55 +0200

    Change design of navbar and remove code from HomePage.scss

--- client/src/components/pages/HomePage/HomePage.js
+++ client/src/components/pages/HomePage/HomePage.js
@@ -1,10 +1,11 @@
 import { TechnologiesCarousel } from "../../features/TechnologiesCarousel/TechnologiesCarousel";
 import { TechnologyData } from "../../features/TechnologiesCarousel/TechnologyData";
 import AdminData from "../../features/AdminData/AdminData";
+import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className={styles.root}>
       <AdminData />
       <TechnologiesCarousel technologies={TechnologyData} />
     </div>
