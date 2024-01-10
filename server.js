989c239385a07bf9727d0cbc4c4733b699bd1f09
mKusztal <maciej.kusztal1995@gmail.com>
2024-01-11 00:47:31 +0200

    Add helmet and update mongodb

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const helmet = require("helmet");
 // const https = require('https');
 // const fs = require('fs');
 const mongoose = require("mongoose");
@@ -16,6 +17,10 @@ let uriDB = process.env.DB_URI;
 let secretKey = process.env.EXPRESS_SESSION_SECRET;
 
 const app = express();
+
+// ################
+// install helmet package and secure the code
+// ################
 
 // const options = {
 //   key: fs.readFileSync(path.join(__dirname, "ca-key.pem")),
@@ -62,6 +67,7 @@ app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "/client/build")));
+app.use(helmet());
 app.use(
   session({
     secret: secretKey,
